8845de33c545144c0333e741de533f98ab75d3f4
RonieJAssis <ronie.gremio@gmail.com>
2016-12-03 00:26:49 -0200

    FASE5

---
+++ Assets/Esmagar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Esmagar : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || (other.gameObject.CompareTag("tiro")))
+        {
+            Destroy(other.gameObject);
+
+        }
+    }
+}
